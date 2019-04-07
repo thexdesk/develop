3c262bc22370c58b144e3f5fb819a11689a05e06
Robin Radic <robin@radic.nl>
2019-04-07 20:15:41 +0200

    test jenkins backend serve

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -10,13 +10,11 @@ MYDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ROOT_DIR=$(dirname $MYDIR)
 THEME_DIR="$ROOT_DIR/theme"
 
+alias lv=php "$ROOT_DIR/artisan"
 
 
 function _load-dotenv {
     source $ROOT_DIR/.env
-}
-function _lv {
-    php $ROOT_DIR/artisan $*
 }
 function _getip {
     $ROOT_DIR/node_modules/.bin/internal-ip --ipv4
@@ -26,23 +24,25 @@ function _git-submodule-update {
     pwd
     git submodule update --init --remote --force
 }
-function _backend-install-codex{
-    _lv codex:addon:enable codex/auth
-    _lv codex:addon:enable codex/comments
-    _lv codex:addon:enable codex/filesystems
-    _lv codex:addon:enable codex/git
-    _lv codex:addon:enable codex/packagist
-    _lv codex:addon:enable codex/phpdoc
-    _lv codex:addon:enable codex/sitemap
+function _backend-install-codex {
+
+    lv codex:addon:enable codex/auth
+    lv codex:addon:enable codex/comments
+    lv codex:addon:enable codex/filesystems
+    lv codex:addon:enable codex/git
+    lv codex:addon:enable codex/packagist
+    lv codex:addon:enable codex/phpdoc
+    lv codex:addon:enable codex/sitemap
+    
 }
 function _backend-install-env {
     local IP=$(getip)
     cp -f $ROOT_DIR/.env.jenkins $ROOT_DIR/.env
-    _lv dotenv:set-key APP_URL $IP
-    _lv dotenv:set-key BACKEND_HOST $IP
-    _lv dotenv:set-key BACKEND_PORT $BACKEND_PORT
-    _lv dotenv:set-key BACKEND_URL $IP:$BACKEND_PORT
-    _lv key:generate
+    lv dotenv:set-key APP_URL $IP
+    lv dotenv:set-key BACKEND_HOST $IP
+    lv dotenv:set-key BACKEND_PORT $BACKEND_PORT
+    lv dotenv:set-key BACKEND_URL $IP:$BACKEND_PORT
+    lv key:generate
 }
 
 
@@ -75,7 +75,7 @@ frontend-build(){
 
 backend-serve(){
     _load-dotenv
-    _lv serve --host=$BACKEND_HOST --port=$BACKEND_PORT
+    lv serve --host=$BACKEND_HOST --port=$BACKEND_PORT
 }
 
 $*
