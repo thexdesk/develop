39b0074be7d6f616a5aaf1f5d4faa13cff8b8d5d
Robin Radic <robin@radic.nl>
2019-04-17 10:50:49 +0200

    upd

--- scripts/untag.sh
+++ scripts/untag.sh
@@ -1,22 +1,34 @@
 #!/usr/bin/env bash
-git tag -d auth/0.1.0
-git tag -d auth/0.2.0
-git tag -d blog/0.1.0
-git tag -d comments/0.1.0
-git tag -d composer-plugin/1.0.0
-git tag -d filesystems/0.1.0
-git tag -d git/1.0.0
-git tag -d packagist/0.1.0
-git tag -d phpdoc/0.1.0
-git tag -d sitemap/0.1.0
+#git tag -d auth/0.1.0
+#git tag -d auth/0.2.0
+#git tag -d blog/0.1.0
+#git tag -d comments/0.1.0
+#git tag -d composer-plugin/1.0.0
+#git tag -d filesystems/0.1.0
+#git tag -d git/1.0.0
+#git tag -d packagist/0.1.0
+#git tag -d phpdoc/0.1.0
+#git tag -d sitemap/0.1.0
 
-git push --delete auth 0.1.0
-git push --delete auth 0.2.0
-git push --delete blog 0.1.0
-git push --delete comments 0.1.0
-git push --delete composer-plugin 1.0.0
-git push --delete filesystems 0.1.0
-git push --delete git 1.0.0
-git push --delete packagist 0.1.0
-git push --delete phpdoc 0.1.0
-git push --delete sitemap 0.1.0
+#git push --delete auth 0.1.0
+#git push --delete auth 0.2.0
+#git push --delete blog 0.1.0
+#git push --delete comments 0.1.0
+#git push --delete composer-plugin 1.0.0
+#git push --delete filesystems 0.1.0
+#git push --delete git 1.0.0
+#git push --delete packagist 0.1.0
+#git push --delete phpdoc 0.1.0
+#git push --delete sitemap 0.1.0
+
+git push --delete algolia-search develop
+git push --delete auth develop
+git push --delete blog develop
+git push --delete comments develop
+git push --delete composer-plugin develop
+git push --delete core develop
+git push --delete filesystems develop
+git push --delete git develop
+git push --delete packagist develop
+git push --delete phpdoc develop
+git push --delete sitemap
